61ef9757eb535bc282b516f9141315e91ed9313f
Owen Middleton <om221@sussex.ac.uk>
2020-09-29 15:25:45 +0100

    Commit

--- Tutorials/Occurrence maps using GBIF and flickr/Code/Occurrence maps using GBIF and flickr.R
+++ Tutorials/Occurrence maps using GBIF and flickr/Code/Occurrence maps using GBIF and flickr.R
@@ -7,35 +7,6 @@
 # Visualising and creating heat maps of puffin occurrence records in GBIF (Global Biodiversity Information Facility) 
 # and from online images on Flickr
 
-# Presented by: Owen Middleton
-
-
-# July 2020 paper in Trends in Ecology and Evolution:
-# iEcology: Harnessing Large Online Resources to Generate Ecological Insights
-# Access: https://www.sciencedirect.com/science/article/pii/S016953472030077X
-
-# Abstract:
-# iEcology is a new research approach that seeks to quantify patterns and processes in the natural world
-# using data accumulated in digital sources collected for other purposes.
-
-# iEcology studies have provided new insights into species occurrences, traits, phenology, functional
-# roles, behavior, and abiotic environmental features.
-
-# iEcology is expanding, and will be able to provide valuable support for ongoing research efforts, as
-# comparatively low-cost research based on freely available data.
-
-# We expect that iEcology will experience rapid development over coming years and become one of the major
-# research approaches in ecology, enhanced by emerging technologies such as automated content analysis,
-# apps, internet of things, ecoacoustics, web scraping, and open source hardware.
-
-
-
-# Example of Flickr use in iEcology
-# Increasing records of non-native reptile species in the UK:
-# Author: Stephen Allain (PhD Student at the University of Kent)
-# https://www.researchgate.net/profile/Steven_Allain/publication/334138130_Mining_Flickr_a_method_for_expanding_the_known_distribution_of_invasive_species/links/5d19fb5f92851cf4405a54d5/Mining-Flickr-a-method-for-expanding-the-known-distribution-of-invasive-species.pdf 
-
-
 #————————————————————————————————————————————————————————————————————————————————————————————####
 # Main tutorial ----
 #————————————————————————————————————————————————————————————————————————————————————————————####
@@ -209,10 +180,10 @@ flickr.density <- ggplot(data = flickr.points, aes(x = longitude, y = latitude))
 
 # Save plots ----
 ggsave("./Tutorials/Occurrence maps using GBIF and flickr/Results/FlickR Density Map.tiff",
-       flickr.density, height = 6, width = 6, units = "in", dpi = 72)
+       flickr.density, height = 10, width = 10, units = "in", dpi = 150)
 
 ggsave("./Tutorials/Occurrence maps using GBIF and flickr/Results/GBIF Density Map.tiff",
-       gbif.density, height = 6, width = 6, units = "in", dpi = 72)
+       gbif.density, height = 10, width = 10, units = "in", dpi = 150)
 
 
 #————————————————————————————————————————————————————————————————————————————————————————————####
